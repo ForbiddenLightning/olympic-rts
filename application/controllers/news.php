d2a29a76be8c784558b7f63e662ace626d552d4d
Jack Hodkinson <jhodkinson80@gmail.com>
2012-08-07 19:30:16 +0100

    Removed duplicate function

--- application/controllers/news.php
+++ application/controllers/news.php
@@ -6,13 +6,7 @@ class News extends CI_Controller {
 		$this->load->model('news_model');
 	}
 
-	public function index()
-	{
-		$data['news'] = $this->news_model->get_news();
-	}
-
-	public function view($slug)
-	{
+	public function view($slug) {
 		$data['news'] = $this->news_model->get_news($slug);
 	}
 
