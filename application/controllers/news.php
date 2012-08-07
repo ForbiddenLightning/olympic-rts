0bad6c3c80ad579c40520df0dff543ac6209857a
Jack Hodkinson <jhodkinson80@gmail.com>
2012-08-07 19:28:49 +0100

    Fixed parse error

--- application/controllers/news.php
+++ application/controllers/news.php
@@ -1,8 +1,7 @@
 <?php
 class News extends CI_Controller {
 
-	public function __construct()
-	{
+	public function __construct() {
 		parent::__construct();
 		$this->load->model('news_model');
 	}
@@ -16,29 +15,29 @@ class News extends CI_Controller {
 	{
 		$data['news'] = $this->news_model->get_news($slug);
 	}
-}
 
-public function index() {
-	$data['news'] = $this->news_model->get_news();
-	$data['title'] = 'News archive';
-
-	$this->load->view('templates/header', $data);
-	$this->load->view('news/index', $data);
-	$this->load->view('templates/footer');
-}
-
-public function view($slug) {
-	$data['news_item'] = $this->news_model->get_news($slug);
-
-	if (empty($data['news_item']))
-	{
-		show_404();
+	public function index() {
+		$data['news'] = $this->news_model->get_news();
+		$data['title'] = 'News archive';
+	
+		$this->load->view('templates/header', $data);
+		$this->load->view('news/index', $data);
+		$this->load->view('templates/footer');
 	}
-
-	$data['title'] = $data['news_item']['title'];
-
-	$this->load->view('templates/header', $data);
-	$this->load->view('news/view', $data);
-	$this->load->view('templates/footer');
+	
+	public function view($slug) {
+		$data['news_item'] = $this->news_model->get_news($slug);
+	
+		if (empty($data['news_item']))
+		{
+			show_404();
+		}
+	
+		$data['title'] = $data['news_item']['title'];
+	
+		$this->load->view('templates/header', $data);
+		$this->load->view('news/view', $data);
+		$this->load->view('templates/footer');
+	}
 }
 ?>
