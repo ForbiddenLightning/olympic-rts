6c31a562993dc039bf8b5cfb90000c0fa4b57e66
Jack Hodkinson <jhodkinson80@gmail.com>
2012-08-08 11:03:25 +0100

    Fixed PHP Error in view.php

--- application/views/news/view.php
+++ application/views/news/view.php
@@ -1,29 +1,10 @@
-echo '<h2>'.$news_item['title'].'</h2>';
-echo $news_item['text'];
-?>
-<div id="box">
+				<div id="box">
 					<div class="box">
-						<h2><?php
-                                                echo $news_item['title'];
-                                            
-                                                ?></h2>
+						<h2><?php echo $news_item['title']; ?></h2>
 						<img class="alignleft" src="images/pic01.jpg" alt="" />
 						<p>
-							<?php
-                                                echo $news_item['text'];
-                                            
-                                                ?>
+							<?php echo $news_item['text']; ?>
 						</p>
 					</div>
-					<?php foreach ($news as $news_item): ?>
-						<div class="box">
-						<h3><?php echo $news_item['title'] ?></h3>
-						<p>
-        					<?php echo $news_item['text'] ?>
-    					</p>
-    					<ul class="list">
-							<li class="last"><a href="news/<?php echo $news_item['slug'] ?>">View article</a></li>
-						</ul>
-						<?php endforeach ?>
 					<br class="clearfix" />
-				       </div>
+				</div>
