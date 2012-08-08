990bd2aabcdd05d0a2bb8d9a6ec21b4243b330b3
Jack Hodkinson <jhodkinson80@gmail.com>
2012-08-08 09:44:53 +0100

    .

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,8 +1,9 @@
 <html>
 <head>
 	<title><?php echo $title; ?></title>		
-	<link rel="stylesheet" type="text/css" href="<?php echo (base_url() . 'assets/css/prolific.css') ;  ?>" />
+	<!--<link rel="stylesheet" type="text/css" href="<?php echo (base_url() . 'assets/css/prolific.css') ;  ?>" />-->
 </head>
 <body>
 	<h1>Olympic RTS</h1>
+	<?php echo 'hello' ; ?>
 	<?php echo base_url(); ?>
