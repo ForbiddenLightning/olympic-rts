26adafd97fbe6ef2dfc63dfd7ed5a5270c11b953
Jack Hodkinson <jhodkinson80@gmail.com>
2012-08-08 09:53:09 +0100

    updated header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,9 +1,8 @@
 <html>
 <head>
 	<title><?php echo $title; ?></title>		
-	<!--<link rel="stylesheet" type="text/css" href="<?php echo (base_url() . 'assets/css/prolific.css') ;  ?>" />-->
+	<link rel="stylesheet" type="text/css">@import url('assets/css/prolific.css');</link>
 </head>
 <body>
 	<h1>Olympic RTS</h1>
 	<?php echo 'hello' ; ?>
-	<?php echo base_url(); ?>
