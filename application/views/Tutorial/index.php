8a05c62c6efec3c26550bc8eb753c096b4cc8dad
evan swift <ev.ms@hotmail.com>
2012-08-08 15:08:40 +0100

    JACK APPLY

---
+++ application/views/Tutorial/index.php
@@ -0,0 +1,24 @@
+				<div id="content">
+					<div class="box">
+						<h2>Tutorial</h2>
+						<img class="alignleft" src="<?php echo base_url() . 'css/images/logo.jpg'; ?>" alt="" />
+						<p>This Is Olympic RTS. A Game Based around building up your countrys team based around real time data. 
+						</p>
+<p>
+You will start with 1000 gold. This can be Used to build a base team of athletes. what you do with them you can choose. whether it be 1 super athlete with a higher level or many lower skilled athletes.
+</p>
+<p>
+fortnightly the games will take place and you can compete with real players from around the world to win in a range of events where you can use the athletes you have made.
+</p>
+<p>
+another aspect of winnings apart from gold is XP. XP can be used to ethier buy gold or increase the overall level of your team.
+</p>
+<p>. 
+A gold medal is worth=100G + 100xp. Silver=50G + 75xp. Bronze=25G + 50XP.
+and anything else is 10G + 10XP.
+</p>
+
+<p>
+if you team is good enough then you can qualify for global ranking.
+</p>
+					
---
+++ application/views/Tutorial/view.php
@@ -0,0 +1,10 @@
+				<div id="box">
+					<div class="box">
+						<h2><?php echo $news_item['title']; ?></h2>
+						<img class="alignleft" src="images/pic01.jpg" alt="" />
+						<p>
+							<?php echo $news_item['text']; ?>
+						</p>
+					</div>
+					<br class="clearfix" />
+				</div>
