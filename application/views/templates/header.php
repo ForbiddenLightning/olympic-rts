1d7f2e69741ca5f589099cc72d0cfa4270bfd6dd
Jack Hodkinson <jhodkinson80@gmail.com>
2012-08-08 10:02:00 +0100

    CSS Paths

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,9 +1,9 @@
 <?php $this->load->helper('url'); ?>
 <html>
 <head>
-	<title><?php echo $title; ?></title>		
+	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() . 'ci/css/prolific.css'; ?>" />	
 </head>
 <body>
 	<h1>Olympic RTS</h1>
-	<?php echo site_url() ; ?>
-	<?php echo site_url('assets/css/prolific.css') ; ?>
+	<?php echo site_url('ci/css/prolific.css') ; ?>
