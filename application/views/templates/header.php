dc57c73014f1d6844c7d0133e58c8074542cc06e
Jack Hodkinson <jhodkinson80@gmail.com>
2012-08-08 09:41:29 +0100

    debug

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,6 +2,7 @@
 <head>
 	<title><?php echo $title ?></title>		
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url() . 'assets/css/prolific.css';  ?>" />
+	<?php echo base_url() ?>
 </head>
 <body>
 	<h1>Olympic RTS</h1>
