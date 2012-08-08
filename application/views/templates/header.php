d86ce4cd81051a9adc8ec2cc9198104cd946b351
Jack Hodkinson <jhodkinson80@gmail.com>
2012-08-08 09:40:38 +0100

    Moved CSS

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 	<title><?php echo $title ?></title>		
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url() .“assets/css/prolific.css”  ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() . 'assets/css/prolific.css';  ?>" />
 </head>
 <body>
 	<h1>Olympic RTS</h1>
