10ec312f9e73c657ad22d6de1ab0a3a28aed6cda
Eric Moon <agrikid1@gmail.com>
2012-08-08 14:43:00 +0100

    updated the site.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -50,11 +50,11 @@
 						</p>
 					</div>
 					<div class="box">
-						<h3>More Links</h3>
+						<h3>YRS and other links</h3>
 						<ul class="list">
 							<li class="first"><a href="#">These</a></li>
-							<li><a href="#">Can</a></li>
-							<li><a href="#">Also</a></li>
+							<li><a href="http://www.youngrewiredstate.org/"></a>YRS Site</li>
+							<li><a href="#"></a></li>
 							<li><a href="#">Be</a></li>
 							<li class="last"><a href="#">Edited</a></li>
 						</ul>
